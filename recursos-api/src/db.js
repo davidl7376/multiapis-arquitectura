5973e23b6792518557f094045a8e6f55eeb765e9
davidl7376 <davidlozada7376@gmail.com>
2025-11-20 01:25:46 -0500

    Prueba 6

--- recursos-api/src/db.js
+++ recursos-api/src/db.js
@@ -1,10 +1,6 @@
 import pg from "pg";
 
 export const pool = new pg.Pool({
-  user: 'postgres',
-  host: 'postgres',  // ← nombre del servicio en Docker
-  database: 'multiapisdb',
-  password: 'password',
-  port: 5432,
-  ssl: false  // ← false en lugar del objeto
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
